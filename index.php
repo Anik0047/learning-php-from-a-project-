5a3c6b2ec1ea97526643a4d5265a7c06221a477b
Anik0047 <clananik@gmail.com>
2024-01-10 21:09:50 +0600

    done-ui

--- index.php
+++ index.php
@@ -86,6 +86,28 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img class="card-img-top" src="./image/section-4/shirt-blue-1.jpg" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Add to cart</a>
+                                    <a href="#" class="btn btn-secondary">View more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img class="card-img-top" src="./image/section-4/shirt-blue-1.jpg" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Add to cart</a>
+                                    <a href="#" class="btn btn-secondary">View more</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <!-- side nav -->
@@ -109,7 +131,7 @@
                             <a href="" class="nav-link text-light">Brand4</a>
                         </li>
                     </ul>
-                    
+
                     <!-- categories part -->
                     <ul class="navbar-nav me-auto text-center">
                         <li class="nav-item bg-info">
